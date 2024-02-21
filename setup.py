0a5a5c564c2e747f621b3917063ac538c6631822
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-02-21 17:00:36 +0100

    initial version

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="generic_app",
+    version="1.0.0",
+    author="Melih Sünbül",
+    author_email="m.sunbul@lund-it.com",
+    description="A Python / Django library to create business applications easily with complex logic",
+    long_description_content_type="text/markdown",
+    url="https://github.com/LundIT/dpag-generic-app",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
