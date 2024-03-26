31e129c1964fcccf0869b4f5d61b92fc1845c0c9
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-03-26 14:24:07 +0100

    Revert "removed unnecessary imports and usages"
    This reverts commit 47ffb288972d7049890eda60be38e7d85c4c8c4f.

--- generic_app/rest_api/signals.py
+++ generic_app/rest_api/signals.py
@@ -1,5 +1,6 @@
 import os
 
+from generic_app.rest_api.calculated_model_updates.update_handler import CalculatedModelUpdateHandler
 from django.dispatch import receiver
 
 from django.db.models.signals import post_save
@@ -21,6 +22,7 @@ def calculation_logs(sender, instance, created, **kwargs):
 @receiver(post_save)
 def send_calculation_notification(sender, instance, created, **kwargs):
     from generic_app.submodels.CalculationLog import CalculationLog
+    # from generic_app.submodels.Notifications import Notifications
     if created and sender == CalculationLog and instance.is_notification:
         channel_layer = get_channel_layer()
         message = {
@@ -30,9 +32,24 @@ def send_calculation_notification(sender, instance, created, **kwargs):
                 'message': instance.message,
             }
         }
+        # notification = Notifications(message=instance.message, timestamp=datetime.now())
+        # notification.save()
         async_to_sync(channel_layer.group_send)(f'calculation_logs_{os.getenv("DOMAIN_HOSTED", "localhost")}', message)
 
+# @receiver(post_save)
+# def send_notification(sender, instance, created, **kwargs):
+#     from generic_app.submodels.Notifications import Notifications
+#
+#     if created and sender == Notifications:
+#         channel_layer = get_channel_layer()
+#         message = {
+#             'type': 'notifications',  # This is the correct naming convention
+#             'payload': Notifications.objects.values()
+#         }
+#         async_to_sync(channel_layer.group_send)(f'notifications_{os.getenv("DOMAIN_HOSTED", "localhost")}', message)
+
 def update_calculation_status(instance):
+        # from generic_app.submodels.Notifications import Notifications
         channel_layer = get_channel_layer()
         message = {
             'type': 'calculation_is_completed', # This is the correct naming convention
@@ -40,6 +57,8 @@ def update_calculation_status(instance):
                 'record': str(instance),
             }
         }
+        # notification = Notifications(message="Calculation is finished", timestamp=datetime.now())
+        # notification.save()
         async_to_sync(channel_layer.group_send)(f'update_calculation_status_{os.getenv("DOMAIN_HOSTED", "localhost")}', message)
 
 def get_model_data(calculationId):
@@ -53,6 +72,9 @@ def get_model_data(calculationId):
     for message in queryset_calc:
         messages += str(message.timestamp) + ' ' + str(message.message) + '\n'
     return messages
+
+def do_post_save(sender, **kwargs):
+    CalculatedModelUpdateHandler.register_save(kwargs['instance'])
 
 from django.dispatch import Signal
 
