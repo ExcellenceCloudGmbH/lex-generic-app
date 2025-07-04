77132a9fb38fa45a7aa5fc70829c1139f3680d1c
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2025-07-04 13:15:32 +0200

    added choices support to char fields

--- generic_app/rest_api/views/model_info/Fields.py
+++ generic_app/rest_api/views/model_info/Fields.py
@@ -1,5 +1,9 @@
-from django.db.models import ForeignKey, IntegerField, FloatField, BooleanField, DateField, DateTimeField, FileField, \
-    ImageField, AutoField, JSONField, ManyToManyField
+from django.db.models import (
+    ForeignKey, ManyToManyField, CharField,
+    IntegerField, FloatField, BooleanField,
+    DateField, DateTimeField, FileField, ImageField,
+    AutoField, JSONField
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -14,6 +18,8 @@ from generic_app.rest_api.views.permissions.UserPermission import UserPermission
 
 DJANGO_FIELD2TYPE_NAME = {
     ForeignKey: 'foreign_key',
+    ManyToManyField: 'many_to_many',
+    CharField: 'string',          # explicitly support CharField
     IntegerField: 'int',
     FloatField: 'float',
     BooleanField: 'boolean',
@@ -28,7 +34,6 @@ DJANGO_FIELD2TYPE_NAME = {
     CalculateField: 'calculate',
     IsCalculatedField: 'is_calculated',
     JSONField: 'json',
-    ManyToManyField: 'many_to_many',
 }
 
 DEFAULT_TYPE_NAME = 'string'
@@ -39,21 +44,30 @@ def create_field_info(field):
         default_value = field.get_default()
 
     field_type = type(field)
+    type_name = DJANGO_FIELD2TYPE_NAME.get(field_type, DEFAULT_TYPE_NAME)
 
     additional_info = {}
-    if field_type == ForeignKey or field_type == ManyToManyField:
-        additional_info['target'] = field.target_field.model._meta.model_name
-        # Get limit_choices_to information
-        limit_choices_to = field.remote_field.limit_choices_to
-        additional_info['limit_choices_to'] = limit_choices_to
+
+    # relations: include target and any limit_choices_to
+    if field_type in (ForeignKey, ManyToManyField):
+        additional_info['target'] = field.remote_field.model._meta.model_name
+        additional_info['limit_choices_to'] = field.remote_field.limit_choices_to
+
+    # choices (for CharField or any field with choices)
+    if getattr(field, 'choices', None):
+        # turn each (value, label) into a dict for clarity
+        additional_info['choices'] = [
+            {'id': val, 'name': label}
+            for val, label in field.choices
+        ]
 
     return {
         'name': field.name,
         'readable_name': field.verbose_name.title(),
-        'type': DJANGO_FIELD2TYPE_NAME.get(field_type, DEFAULT_TYPE_NAME),
-        # we assume that auto-fields should not be edited
-        'editable': field.editable and not field_type == AutoField,
-        'required': not (field.null or default_value),
+        'type': type_name,
+        # auto-fields (AutoField) are not editable
+        'editable': field.editable and field_type is not AutoField,
+        'required': not (field.null or default_value is not None),
         'default_value': default_value,
         **additional_info
     }
@@ -66,11 +80,8 @@ class Fields(APIView):
     def get(self, *args, **kwargs):
         model = kwargs['model_container'].model_class
         fields = list(model._meta.fields) + list(model._meta.many_to_many)
-        field_info = {'fields': [
-            create_field_info(field) for field in fields
-        ], 'id_field': model._meta.pk.name}
-        # TODO maybe: also send an array containing only those fields that should be presented in the table
-        #   to the frontend. This is configured in the model-process-admin-class for the model (which can
-        #   be accessed via the model_container) --> The idea is, that the table only shows the main fields
-        #   but avoids unnecessary information
+        field_info = {
+            'fields': [create_field_info(f) for f in fields],
+            'id_field': model._meta.pk.name
+        }
         return Response(field_info)
