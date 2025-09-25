2723597674c7ca6bf9cf17cae951967454f2daf7
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2025-09-25 16:20:12 +0200

    updated package version

--- generic_app/rest_api/views/utils.py
+++ generic_app/rest_api/views/utils.py
@@ -1,6 +1,8 @@
 def get_user_name(request):
     if "JIRA" in request.headers:
         return "Created by Jira"
+    elif "api-key" in [header.lower() for header in list(request.headers)]:
+        return "Created by Technical User"
     return f"{request.auth['name']} ({request.auth['sub']})"
 
 
